625c4b64242a3f46456b427137f3a67432255093
Nijoy P Jose <160378117+NIJOY-P-JOSE@users.noreply.github.com>
2026-06-06 11:56:27 +0530

    Implement method to count numbers with even digits

---
+++ 1295. Find Numbers with Even Number of Digits/Solution.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int findNumbers(int[] nums) {
+
+        int c=0,count;
+        for(int i=0;i<nums.length;i++)
+        {
+            int n = nums[i];
+            count = 0;
+            while(n!=0)
+            {
+                n=n/10;
+                count++;
+            }
+            if(count%2==0)
+                c++;
+        } 
+        return c;
+    }
+}
