18581291247b1dc869755df8e73de4c3f1b6726a
Nijoy P Jose <160378117+NIJOY-P-JOSE@users.noreply.github.com>
2026-07-04 20:40:14 +0530

    Add Java program for right-aligned half pyramid

---
+++ Pattern-Problems/Right Aligned Half Pyramid/Solution.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
+        int n = s.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            int stars = i + 1;
+
+            for (int j = 0; j < n - stars; j++)
+                System.out.print("  ");
+
+            for (int j = 0; j < stars; j++)
+                System.out.print("* ");
+
+            System.out.println();
+        }
+    }
+}
