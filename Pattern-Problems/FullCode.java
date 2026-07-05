ee904595335b3ac45cef702305793f9a607994a1
Nijoy P Jose <160378117+NIJOY-P-JOSE@users.noreply.github.com>
2026-07-05 13:08:44 +0530

    Implement various pattern printing in FullCode.java
    Added multiple patterns to be printed based on the input size, enhancing the functionality of the program.

--- Pattern-Problems/FullCode.java
+++ Pattern-Problems/FullCode.java
@@ -81,5 +81,66 @@ class Main {
         for(int i = 0;i<n+n-1;i++)
             System.out.print("*");
         System.out.println();
+        
+        
+        for(int i = 0;i<n+n-1;i++)
+            System.out.print("*");
+        System.out.println();
+        for(int i = 1;i<n;i++)
+        {
+            c = n-i;
+            for(int j = 0;j<n+c;j++)
+                if(n-c==j || n+c-2==j)
+                    System.out.print("*");
+                else
+                    System.out.print(" ");
+            System.out.println();
+        }
+        System.out.println("\n");
+        
+        
+        c = 0;
+        for(int i = 0;i<n+n-1;i++)
+        {
+            if(i<n)
+                c = i;
+            else
+                c = c-1;
+            for(int j = 0;j<n+c;j++)
+                if(n-1-c==j || n-1+c==j)
+                    System.out.print("*");
+                else
+                    System.out.print(" ");
+            
+            System.out.println();
+        }
+        System.out.println();
+        
+        
+        
+        for(int i =0;i<=2*n;i++)
+        {
+            for(int j = 0;j<=2*n;j++)
+            {
+                c=Math.min(Math.min(i,j),Math.min(2*n-i,2*n-j));;
+                System.out.print(c+" ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+        
+        
+        
+    
+        for(int i =1;i<2*n;i++)
+        {
+            for(int j = 1;j<2*n;j++)
+            {
+                c=n-(Math.min(Math.min(i,j),Math.min(2*n-i,2*n-j)))+1;
+                System.out.print(c+" ");
+            }
+            System.out.println();
+        }
+        System.out.println();
     }
 }
