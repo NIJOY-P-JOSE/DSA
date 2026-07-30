d23bc7f497f0cc6b719a2fdb81e7db30df508073
Nijoy P Jose <160378117+NIJOY-P-JOSE@users.noreply.github.com>
2026-07-30 14:12:57 +0530

    Implement checkIfExist method in Solution class

---
+++ Hash Table/1346. Check If N and Its Double Exist/Solution.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean checkIfExist(int[] arr) {
+        HashSet<Integer> set = new HashSet<>();
+
+        for(int i: arr)
+        {
+            if(set.contains(2*i))
+                return true;
+            
+            if(i%2==0 && set.contains(i/2))
+                return true;
+            
+            set.add(i);
+        }
+        return false;
+    }
+}
