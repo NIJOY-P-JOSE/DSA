bfa195c8dbed2d435f5d8a18f889deb4578f0aae
Nijoy P Jose <160378117+NIJOY-P-JOSE@users.noreply.github.com>
2026-08-27 10:59:54 +0530

    Add coinChange method to solve Coin Change problem
    Implement dynamic programming solution for coin change problem.

---
+++ Dynamic Programming (DP)/322. Coin Change/Solution.py
@@ -0,0 +1,15 @@
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+
+        dp = [float('inf')] * (amount + 1)
+        dp[0] = 0
+
+        for i in range(len(dp)):
+            for c in coins:
+                if c <= i:
+                    dp[i] = min(dp[i], dp[i-c] + 1)
+
+        if dp[amount] == float('inf'):
+            return -1
+
+        return dp[amount]
