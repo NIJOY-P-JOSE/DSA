164662fe47e7ff4847cee3b3aa509df4e14f4667
Nijoy P Jose <160378117+NIJOY-P-JOSE@users.noreply.github.com>
2026-09-25 20:29:12 +0530

    Fix right pointer initialization in maxArea method

--- Two Pointers/11. Container With Most Water/Solution.java
+++ Two Pointers/11. Container With Most Water/Solution.java
@@ -1,13 +1,14 @@
 class Solution {
     public int maxArea(int[] height) {
 
-        int maxWater = 0, n =height.length;
-        int left = 0, right = n-1;
+        int maxWater = 0;
+        int left = 0, right = height.length;
         
         while(left<right){
 
             int area = (right-left) * Math.min(height[left],height[right]);
             //         ^--Width--^    ^---------------Height--------------^
+            
             maxWater = Math.max(maxWater,area);
 
             if(height[left]>height[right])
@@ -15,6 +16,7 @@ class Solution {
             else
                 left++;
         }
+        
         return maxWater;
     }
 }
