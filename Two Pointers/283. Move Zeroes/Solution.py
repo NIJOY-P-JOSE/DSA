87bfd2d0b7f444ebb3e7b5e74f19b1430a14d1f2
Nijoy P Jose <160378117+NIJOY-P-JOSE@users.noreply.github.com>
2026-09-26 20:22:12 +0530

    Add moveZeroes function to rearrange zeroes
    Implement function to move all zeroes in an array to the end while maintaining the order of non-zero elements.

---
+++ Two Pointers/283. Move Zeroes/Solution.py
@@ -0,0 +1,18 @@
+void moveZeroes(int* nums, int numsSize) {
+
+    int i, j = 0;
+
+    for (i = 0; i < numsSize; i++) {
+
+        if (nums[i] != 0) {
+
+            nums[j] = nums[i];
+            j++;
+        }
+    }
+
+    while (j < numsSize) {
+
+        nums[j++] = 0;
+    }
+}
